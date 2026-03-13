e69cb484d2c32997c3ae2878e3aa40bb9dd662fb
Jason <jason13.deng@gmail.com>
2026-03-13 14:35:30 +0900

    feat: validation for sign up

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import { prisma } from '../lib/prisma.js';
-import passport from '../config/passport.js'
+import passport from '../config/passport.js';
 import bcrypt from 'bcryptjs';
+import { body, validationResult } from 'express-validator';
 
 export const createUserGet = async (req, res, next) => {
 	try {
@@ -10,21 +11,58 @@ export const createUserGet = async (req, res, next) => {
 	}
 };
 
-export const createUserPost = async (req, res, next) => {
-	try {
-		const hashedPassword = await bcrypt.hash(req.body.password, 10);
-		await prisma.user.create({
-			data: {
-				username: req.body.username,
-				password: hashedPassword,
-			},
-		});
-		res.redirect('/');
-		console.log('created user' + `${req.body.username}`);
-	} catch (err) {
-		next(err);
-	}
-};
+const alphaErr = 'must only contain letters.';
+const lengthErr = 'must be between 1 and 10 characters.';
+const emptyErr = 'is required.';
+
+const validateSignUp = [
+	body('username')
+		.trim()
+		.notEmpty()
+		.withMessage(`Username ${emptyErr}`)
+		.custom(async (value, { req }) => {
+			const foundUser = await prisma.user.findUnique({
+				where: {username: req.body.username},
+			})
+			if (foundUser) {
+				throw new Error('Username taken');
+			}
+			return true;
+		}),
+	body('password')
+		.trim()
+		.notEmpty()
+		.withMessage(`Password ${emptyErr}`),
+];
+
+export const createUserPost = [
+	validateSignUp,
+	async (req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).render('auth/sign-up-form', {
+				title: 'Create user',
+				errors: errors.array(),
+			});
+		}
+		try {
+			const hashedPassword = await bcrypt.hash(
+				req.body.password,
+				10,
+			);
+			await prisma.user.create({
+				data: {
+					username: req.body.username,
+					password: hashedPassword,
+				},
+			});
+			res.redirect('/');
+			console.log('created user' + `${req.body.username}`);
+		} catch (err) {
+			next(err);
+		}
+	},
+];
 
 export const loginGet = async (req, res, next) => {
 	try {
@@ -37,8 +75,8 @@ export const loginGet = async (req, res, next) => {
 export const logoutGet = (req, res, next) => {
 	req.logout((err) => {
 		if (err) {
-			return next(err)
+			return next(err);
 		}
 		res.redirect('/');
-	})
-}
+	});
+};
